9f0e881fd0d597ef987576e436cba4fd9da5775c
dancheung555 <90879895+dancheung555@users.noreply.github.com>
2021-09-16 23:17:32 -0500

    Update BingoCard.java

--- BingoCard.java
+++ BingoCard.java
@@ -1,102 +1,96 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.util.*;
 
-public class BingoCard {
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import java.awt.image.*;
+
+
+
+public class BingoPanel extends JPanel implements KeyListener, MouseListener {
 	
-	private BingoTile[][] tiles = new BingoTile[5][5];
+	private BingoCard card;
+	private Graphics g = null;
 	
-	public BingoCard() {
-		generateTiles();
+	private ArrayList<Integer> allnumbers = new ArrayList<>();
+	
+	public BingoPanel(BingoCard card) {
+		super();
+		this.card = card;
+		
+		addMouseListener(this);
 	}
 	
-	private void generateTiles() {
-		
-		List<Integer> banks0 = new ArrayList<>(15);
-		List<Integer> banks1 = new ArrayList<>(15);
-		List<Integer> banks2 = new ArrayList<>(15);
-		List<Integer> banks3 = new ArrayList<>(15);
-		List<Integer> banks4 = new ArrayList<>(15);
-		
-		for(int i = 1; i <= 15; i++) {
-			banks0.add(i);
-			banks1.add(i+15);
-			banks2.add(i+15*2);
-			banks3.add(i+15*3);
-			banks4.add(i+16*4);
+	public void keyTyped(KeyEvent e) {}
+	public void keyPressed(KeyEvent e) {}
+	public void keyReleased(KeyEvent e) {}
+	public void mousePressed(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
+	public void mouseClicked(MouseEvent e) {
+		System.out.println(g==null);
+		int x = e.getX();
+		int y = e.getY();
+		double distance = Math.sqrt((x-900)*(x-900)+(y-200)*(y-200));
+		if(distance <= 100) {
+			System.out.println(x + " " + y + " " + distance);
+			g.setColor(new Color(0, 125, 125));
 			
 		}
-		
-		BingoTile middle = new BingoTile(-1);
-		middle.setCrossedOff(true);
-		tiles[2][2] = middle;
-		
-		for(int i = 0; i < 5; i++) {
-			int place = random.nextInt(banks0.size());
-			int number = banks0.get(place);
-			banks0.remove(place);
-			BingoTile tile = new BingoTile(number);
-			tiles[0][i] = tile;
-		}
-		
-		for(int i = 0; i < 5; i++) {
-			int place = random.nextInt(banks1.size());
-			int number = banks1.get(place);
-			banks1.remove(place);
-			BingoTile tile = new BingoTile(number);
-			tiles[1][i] = tile;
-		}
-		
-		for(int i = 0; i < 5; i++) {
-			int place = random.nextInt(banks2.size());
-			int number = banks2.get(place);
-			banks2.remove(place);
-			BingoTile tile = new BingoTile(number);
-			tiles[2][i] = tile;
-		}
-		
-		for(int i = 0; i < 5; i++) {
-			int place = random.nextInt(banks3.size());
-			int number = banks3.get(place);
-			banks3.remove(place);
-			BingoTile tile = new BingoTile(number);
-			tiles[3][i] = tile;
-		}
-		
-		for(int i = 0; i < 5; i++) {
-			int place = random.nextInt(banks4.size());
-			int number = banks4.get(place);
-			banks4.remove(place);
-			BingoTile tile = new BingoTile(number);
-			tiles[4][i] = tile;
-		}
-		
+		repaint();
 	}
-	
-	private static Random random;
-	
-	public static void createRandom(long seed) {
-		random = new Random(seed);
-	}
-	
-	private static int getWithinRange(int row) {
-		int number = random.nextInt(15) + 1;
-		return number + (row * 15);
-	}
-	
-	public BingoTile getTile(int x, int y) {
-		return tiles[x][y];
+
+	@Override
+	public void paint(Graphics graphics) {
+		
+		if(g == null) {
+			g = graphics;
+		}
+		
+		graphics.setColor(new Color(255, 255, 255));
+		graphics.fillRect(0,0,2000,2000);
+		
+		try {
+            graphics.drawImage(ImageIO.read(Objects.requireNonNull(getClass().getResource("BingoTemplate.png"))), 0, 0, null);
+            drawText(graphics);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+		
+		//Button
+		graphics.setColor(Color.red);
+		graphics.fillOval(800, 100, 200, 200);			//800-1000, 100-300, so mid at (900, 200)
+//		graphics.drawString("TEST", 900, 75);
+		//Text in button
+		graphics.setColor(Color.white);
+		graphics.setFont(new Font("", Font.PLAIN, 55));
+		graphics.drawString("Ball", 850, 220);
+		
+		//Box of the numbers
+		graphics.setColor(new Color(0, 0, 255));
+		graphics.fillRect(750, 600, 300, 100);
+		
+		//number
+		graphics.setColor(new Color(255, 255, 255));
+		graphics.drawString("12", 860, 600);
+		repaint();
 	}
 	
 	
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		for(int row = 0; row < 5; row++){
+	
+	private void drawText(Graphics numbers) {
+		Font font = new Font("Times New Roman", Font.PLAIN, 40);
+		numbers.setFont(font);
+		for (int row = 0; row < 5; row++) {
 			for (int col = 0; col < 5; col++) {
-				builder.append(tiles[row][col].getValue()).append('\t');
+				if(!(row == 2 && col == 2)) numbers.drawString(Integer.toString(card.getTile(row, col).getValue()), row * 126 + 89, col * 129 + 227);
 			}
-			builder.append('\n');
 		}
-		return builder.toString();
 	}
 }
